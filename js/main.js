ab769afe757e00cb8d6ddc264d54c3b894984540
Bibek Shakya <bshakya@contentformula.com>
2018-06-13 16:32:22 +0100

    finalizing the app

--- js/main.js
+++ js/main.js
@@ -1,11 +1,109 @@
-// show / hide button feature
+/* 
+1. show / hide button 
+-------------------------------------------------------------
+*/
+// create variables
 const toggleBtn = document.querySelector('#toggleBtn');
 const divList = document.querySelector('.listHolder');
 
+// action to be taken when clicked on hide list button
 toggleBtn.addEventListener('click', () => {
   if (divList.style.display === 'none') {
     divList.style.display = 'block';
+    toggleBtn.innerHTML = 'Hide List';
   } else {
     divList.style.display = 'none';
+    toggleBtn.innerHTML = 'Show List';
+  }
+});
+
+/* 
+2. add list items
+-------------------------------------------------------------
+*/
+// create variables
+const addInput = document.querySelector('#addInput');
+const addBtn = document.querySelector('#addBtn');
+
+function addLists() {
+  if (addInput.value === '') {
+    alert('Enter the list name please!!!');
+  } else {
+    const ul = divList.querySelector('ul');
+    const li = document.createElement('li');
+    li.innerHTML = addInput.value;
+    addInput.value = '';
+    ul.appendChild(li);
+    createBtn(li);
+  }
+}
+
+// add list when clicked on add item button
+addBtn.addEventListener('click', () => {
+  addLists();
+});
+
+// add list when pressed enter
+addInput.addEventListener('keyup', (event) => {
+  if(event.which === 13) {
+    addLists();
+  }
+});
+
+/* 
+3. create action buttons
+-------------------------------------------------------------
+*/
+// create variables
+const listUl = document.querySelector('.list');
+const lis = listUl.children;
+
+function createBtn(li) {
+  // create remove button
+  const remove = document.createElement('button');
+  remove.className = 'btn2 remove';
+  li.appendChild(remove);
+
+  // create down button
+  const down = document.createElement('button');
+  down.className = 'btn2 down';
+  li.appendChild(down);
+
+  // create up button
+  const up = document.createElement('button');
+  up.className = 'btn2 up';
+  li.appendChild(up);
+
+  return li;
+}
+
+// loop to add buttons in each li
+for (var i = 0; i < lis.length; i++) {
+  createBtn(lis[i]);
+}
+
+
+/* 
+4. enabling button actions (to move item up, down or delete)
+-------------------------------------------------------------
+*/
+divList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const button = event.target;
+    const li = button.parentNode;
+    const ul = li.parentNode;
+    if (button.className === 'btn2 remove') {
+      ul.removeChild(li);
+    } else if (button.className === 'btn2 down') {
+      const nextLi = li.nextElementSibling;
+      if (nextLi) {
+        ul.insertBefore(nextLi, li);
+      }
+    } else if (button.className === 'btn2 up') {
+      const prevLi = li.previousElementSibling;
+      if (prevLi) {
+        ul.insertBefore(li, prevLi);
+      }
+    }
   }
 });
